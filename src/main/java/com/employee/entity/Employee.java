55ab238c6307ed737f6d68c7739ea0e00cf8b4a6
Posa Karthik <posakarthik16@gmail.com>
2026-06-20 16:36:19 +0530

    Added Employee entity

---
+++ src/main/java/com/employee/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.employee.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long employeeId;
+	private String employeeName;
+	private String employeeEmail;
+	private Double employeeSalary;
+	private String employeeDepartment;
+
+}
