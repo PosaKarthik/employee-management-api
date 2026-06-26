ac4323c8b8b28b6342a81d08ec1fd093e75e8687
Posa Karthik <posakarthik16@gmail.com>
2026-06-27 01:34:26 +0530

    Added ErrorResponse model

---
+++ src/main/java/com/employee/response/ErrorResponse.java
@@ -0,0 +1,26 @@
+package com.employee.response;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+	
+	private String message;
+	private int status;
+	private String error;
+	private String path;
+	private LocalDateTime timestamp;
+	
+	
+
+}
